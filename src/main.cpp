8295bf2b852c9918064124138274e8c70b32647a
lxp <xinhard@qq.com>
2024-01-09 16:24:29 +0800

    cpp Template project.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Filename: d:\Hardware\C\proj\Template\src\main.c
+ * Path: d:\Hardware\C\proj\Template\src
+ * Created Date: Saturday, May 27th 2023, 7:58:10 pm
+ * Author: L xp
+ * 
+ * Copyright (c) 2023 Your Company
+ */
+#include <iostream>
+
+
+unsigned namespace  std;
+
+int main()
+{
+
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
+
+
